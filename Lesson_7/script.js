551f9117f617a278df9543d9a1169aaf82198545
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2022-09-22 22:06:34 +0300

    Lesson_7

---
+++ Lesson_7/script.js
@@ -0,0 +1,128 @@
+// const cats = ['https://i.ytimg.com/vi/iKA6ZXpGcGQ/maxresdefault.jpg', 'https://i.natgeofe.com/n/46b07b5e-1264-42e1-ae4b-8a021226e2d0/domestic-cat_thumb_square.jpg', 'https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg', 'https://i.guim.co.uk/img/media/941093798b256e5d1aa6246a08824bc86f62a31f/0_0_5000_3000/master/5000.jpg?width=1300&quality=85&fit=max&s=ea672fb16df8634901283252059baaf8', 'https://nypost.com/wp-content/uploads/sites/2/2022/06/reddit-cats-judging-looks-00.jpg?quality=75&strip=all', 'https://gdb.voanews.com/09690000-0a00-0242-5408-08da76ebde16_cx0_cy3_cw99_w1200_r1.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCK8yYjQ6igjrAJyCyXUwV6U7RDkLVc4FVnQ&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWEQxVHz2ZzUdIKINgA-LvmuZgoady4hu4hQ&usqp=CAU', 'https://e3.365dm.com/22/07/768x432/skynews-cats-pet_5846440.jpg?20220726201324', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4xXPTgr5Lug8qU_akXp2oC_4m5w3FoAvx_Q&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnB4if5HpwDvEqkgBShHrRvbwYCya8vzB86g&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWEQxVHz2ZzUdIKINgA-LvmuZgoady4hu4hQ&usqp=CAU', 'https://e3.365dm.com/22/07/768x432/skynews-cats-pet_5846440.jpg?20220726201324', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4xXPTgr5Lug8qU_akXp2oC_4m5w3FoAvx_Q&usqp=CAU', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnB4if5HpwDvEqkgBShHrRvbwYCya8vzB86g&usqp=CAU', 'https://assets.globalwildlife.org/m/3b6039a74fd067ea/webimage-Tigrinia.jpg', 'https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a_400x400.jpeg', 'https://www.nhm.ac.uk/content/dam/nhmwww/discover/wild-cats/margay-two-column.jpg.thumb.768.768.jpg', 'https://irs.www.warnerbros.com/gallery-v2-jpeg/cats__dogs_photo_12-14571309.jpg', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIyGk9Ds3bdd4B404EcoMDrVUKnU5u9KABOg&usqp=CAU'];
+
+// console.log(cats.length);
+// // 16
+// const list = document.querySelector('.list');
+
+// const markup = cats.reduce((acc, cat) => acc + `<div>
+// <img loading='lazy' src="${cat}" alt="cat" width='500px'>
+//  <p>ducimus, quasi perferendis.
+//  Eos laudantium optio quasi nam ipsam. Totam dolor perspiciatis cumque corrupti. Dolorem saepe, reprehenderit qui, quibusdam quaerat magni temporibus esse nemo eum, debitis beatae! Fugiat quisquam dolor laborum facere distinctio!
+//  Voluptatibus cumque earum enim! Error id ullam, alias eaque voluptatem enim eos architecto eum ipsa sunt accusantium mollitia iusto deleniti est vero distinctio. Ut impedit quaerat obcaecati vero nobis minus.
+//  Adipisci assumenda nihil porro veniam, laboriosam obcaecati unde fugit? Expedita assumenda neque blanditiis sed repellat officia? Dignissimos saepe veritatis itaque distinctio explicabo quod, et sunt in, incidunt fugiat eos quos!
+//  Id, itaque numquam amet consequuntur accusantium asperiores? Libero deserunt similique eaque, iure, autem et amet ab unde maxime nemo recusandae aspernatur eveniet esse velit ut mollitia distinctio dolorem corrupti! Autem.
+//  Veniam assumenda consequatur optio iure eveniet itaque necessitatibus! Error hic quos voluptas minus. Similique, possimus aut mollitia est accusantium fuga, odit dolores sunt alias iusto vitae veritatis quam expedita libero!
+//  Sint, placeat consectetur autem praesentium ducimus amet nihil ab quae explicabo exercitationem et ipsum. Quasi enim id aperiam aliquam excepturi quo a laborum quisquam expedita. Accusamus ea cumque ratione blanditiis?
+//  Officia, illo eveniet aperiam ab neque dolorum possimus impedit quae repudiandae itaque accusamus amet hic quod mollitia deleniti quisquam illum sapiente dolor sit quam rem laboriosam. Ut cum temporibus cumque.
+//  Iure provident, nisi, accusamus veniam quia quasi deserunt natus quam consectetur dolor modi, veritatis id accusantium facilis voluptatum ad nemo est sapiente ab tempore commodi quod a iste. Eaque, deserunt!
+//  Dolor praesentium enim quisquam eius, eaque officiis tempore quis qui quas tenetur quibusdam! Quo libero vero facilis nulla voluptas. Nesciunt, debitis quod eaque consequuntur veniam asperiores ipsa vero placeat consectetur?
+//  Optio nisi soluta consectetur vitae natus odit, corrupti corporis. Quo unde magnam officiis dolorum quia dolorem minus maxime totam! Nulla quaerat similique neque mollitia est a aut quam enim maxime.
+//  Minima quasi quam qui vero, nihil, nesciunt hic sapiente, corporis debitis praesentium corrupti alias nobis porro? Officiis fuga deserunt recusandae amet numquam atque provident quae, doloremque ipsa impedit repudiandae! Ab!
+//  Et nulla dolorem assumenda earum corrupti consequatur totam. Tenetur, officiis mollitia? Harum vero dolores eum. Enim iure assumenda ipsam dolore saepe itaque, error mollitia ratione corporis, esse, magnam nisi ut!
+//  Recusandae voluptatibus libero a odio consectetur placeat nulla iste molestiae, facilis, ducimus fugiat voluptas repudiandae aperiam iure voluptate? Eaque ex delectus libero odio molestiae vel esse quisquam rem facere quae.
+//  Eligendi, magnam consectetur eius, minima at quibusdam nostrum repudiandae quasi animi ab numquam neque nulla odio ducimus quisquam corporis quos iusto, perspiciatis dolor maiores est. Eveniet iste dolor incidunt eos?
+//  Voluptate voluptatibus, eum, eius laudantium consequatur quas sapiente quibusdam id sint, vero beatae ut. Similique earum autem distinctio labore voluptatem ipsum. Corrupti animi explicabo molestiae magni quod earum omnis voluptatum?
+//  Illo blanditiis sed molestiae similique vel doloremque nemo laboriosam sequi veniam, qui, et fuga saepe, ipsum dolor nobis nesciunt! Nostrum animi provident quibusdam molestias earum voluptates fuga. Eligendi, possimus nisi.
+//  Eius ullam asperiores consequatur? At porro nulla consequuntur eaque maiores tempora corrupti nobis aliquam, molestiae quibusdam error et similique. Asperiores corporis reiciendis illo iusto, impedit animi dolores enim suscipit ipsum!
+//  Harum esse voluptate, officia eum, itaque odio nemo repellat optio blanditiis ducimus perspiciatis quaerat. Omnis, ex. Laborum nihil explicabo autem neque vitae reiciendis. Aperiam culpa magni eveniet, voluptate ad dicta!
+//  Quam corporis itaque voluptates iusto vitae nesciunt repellendus eos, fugiat, accusamus, libero illo natus consequatur numquam ipsam temporibus! Sit possimus, voluptatem suscipit sint reiciendis eaque eveniet nam magni dicta nisi?
+//  Numquam minus at in temporibus fugiat officiis delectus esse error. Sed delectus autem dolorem aliquid, voluptate illo veniam id architecto, nulla cum ex nihil eligendi expedita ab a iste soluta.
+//  Ratione voluptatum provident eaque impedit vel minima repellat distinctio perspiciatis iure, maiores voluptate voluptatem molestiae, atque adipisci saepe deserunt ipsa pariatur. Doloremque error modi voluptates sequi unde aperiam blanditiis fuga!
+//  Laboriosam quam porro delectus ipsa ea doloremque aut tempore iusto perferendis quis ullam odio placeat consectetur a, similique libero saepe numquam necessitatibus ipsum eum cum. Obcaecati iure quasi voluptatem impedit.</p>
+//  </div>`, '')
+// list.insertAdjacentHTML('beforeend', markup)
+
+
+
+
+
+// console.log(list);
+
+
+
+
+
+
+
+
+
+// const input = document.querySelector('.test');
+// console.log(input);
+// input.addEventListener('input', _.debounce(onInput, 3000, {
+//     trailing: false,
+//     leading: true,
+// }));
+
+// function onInput(evt) {
+//     console.log(evt.target.value);
+// }
+
+
+const instruments = [{
+    id: 1,
+    img: 'https://static.dnipro-m.ua/cache/products/1754/catalog_origin_141546.jpg',
+    name: 'Молоток',
+    price: 150
+}, {
+    id: 2,
+    img: 'https://static.dnipro-m.ua/cache/products/5098/catalog_origin_195568.jpg',
+    name: 'Перфоратор',
+    price: 3000
+}, {
+    id: 3,
+    img: 'https://static.dnipro-m.ua/cache/products/2023/catalog_origin_200763.jpg',
+    name: 'Рівень',
+    price: 2000
+}]
+const basket = [];
+const wish = [];
+
+const list = document.querySelector('.list');
+
+const markup = instruments.reduce((acc, instrument) => acc + `<li class='js-item' data-id='${instrument.id}'>
+<img src="${instrument.img}" alt="${instrument.name}"  width="200px">
+<h2>${instrument.name}</h2>
+<p>${instrument.price}</p>
+<button class='js-add'>Add</button>
+<button class='js-wish'>Wish List</button>
+</li>`, "")
+list.insertAdjacentHTML('beforeend', markup)
+
+list.addEventListener('click', onItem)
+
+
+function onItem(evt) {
+    if (evt.target.classList.contains('js-add')) {
+        const toBasket = getProduct(evt.target);
+
+        const productInBasket = basket.find(({
+            id
+        }) => id === currentId);
+
+        if (!productInBasket) {
+            toBasket.qty = 1;
+            toBasket.totalSum = toBasket.price
+            basket.push(toBasket);
+        } else {
+            productInBasket.qty += 1;
+            productInBasket.totalSum = productInBasket.qty * productInBasket.price;
+        }
+        console.log(basket);
+        return;
+    }
+
+    if (evt.target.classList.contains('js-wish')) {
+
+    }
+
+}
+
+
+function getProduct(elem) {
+    const currentProduct = elem.closest('.js-item');
+    const currentId = Number(currentProduct.dataset.id);
+    const product = instruments.find(({
+        id
+    }) => id === currentId);
+    return product;
+}
